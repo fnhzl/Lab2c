e2d96c4281733cf00641f931b8d3df3de6bdd4b7
Farhan Nashrul Ghazali <farhan.nashrul300@gmail.com>
2021-11-25 14:37:10 +0800

    Lab2c

---
+++ app/src/main/java/com/example/lab2c_farhannashrul/FullImageActivity.java
@@ -0,0 +1,23 @@
+package com.example.lab2c_farhannashrul;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class FullImageActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.full_image);
+
+        Intent i = getIntent();
+
+        int position = i.getExtras().getInt("id");
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+
+        ImageView imageView = (ImageView)  findViewById(R.id.full_image_view);
+        imageView.setImageResource(imageAdapter.thumbImages[position]);
+    }
+}
